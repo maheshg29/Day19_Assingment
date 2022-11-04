9c7c10f93c2e8e2cd706788402c6f0ca77b9bf90
mahesh <gangurdemahesh9@gmail.com>
2022-11-04 13:36:10 +0530

    [mahehs]Add UC3 file

--- Day19_Assingment/UserRegistration.cs
+++ Day19_Assingment/UserRegistration.cs
@@ -18,10 +18,17 @@ namespace Day19_Assingment
             }
 
         public static string REGEX_LASTNAME = "^[A-Z]{1}[a-z]{2}[a-z]*";
-        //this function is use for NAME validate
+        //this function is use for LAST NAME validate
         public bool validatelastname(string userdata)
         {
             return Regex.IsMatch(userdata, REGEX_LASTNAME);
+        }
+
+        public static string REGEX_MAILID = "[a-zA-Z0-9_\\-\\.]+[@][a-z]+[\\.][a-z]{2,3}";
+        //this function is use for Mail ID validate
+        public bool validatemail(string userdata)
+        {
+            return Regex.IsMatch(userdata, REGEX_MAILID);
         }
 
 
