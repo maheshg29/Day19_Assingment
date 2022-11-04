8f5a739f51ee5a96083b51019c0328beee50a004
mahesh <gangurdemahesh9@gmail.com>
2022-11-04 13:00:01 +0530

    [mahesh]Add Main Program File

---
+++ Day19_Assingment/Program.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Day19_Assingment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome TO Day 19 assingment Problem");
+            Console.WriteLine("Hello User please Enter the First Name");
+            Console.WriteLine(" NOTE:- First name starts with Cap and haS nminimum 3 characters");
+
+            string userdata = Console.ReadLine();
+            UserRegistration userRegistration = new UserRegistration();
+            bool value = userRegistration.validatename(userdata);
+            
+
+            if (value)
+            {
+                Console.WriteLine("Given Name Accepted");
+
+            }
+            else
+            {
+                Console.WriteLine("Please Enter Valid Name");
+            }
+        }
+    }
+}
+
---
+++ Day19_Assingment/UserRegistration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19_Assingment
+{
+    public class UserRegistration
+    {
+
+        public static string REGEX_NAME = "^[A-Z]{1}[a-z]{2}[a-z]*";
+
+            public bool validatename(string userdata)
+            {
+                return Regex.IsMatch(userdata, REGEX_NAME);
+            }
+        
+    }
+}
